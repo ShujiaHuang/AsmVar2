521a1e8aad91f65513a7ce966bd6eda3b03140e4
Shujia Huang <huangshujia9@gmail.com>
2015-04-28 16:37:40 +0200

    add a value to record the mapped quality

--- asmvar/read.py
+++ asmvar/read.py
@@ -1,15 +1,16 @@
 """
-A class for record reads
+A class for record reads extracted from bamfile
 """
 import pysam
 
 class Read(object):
 
     def __init__(self, read = pysam.AlignedRead()):
-        self.name = read.qname # Read name
-        self.seqs = read.query # The read's sequence
-        self.qual = read.qual  # The read's quality from bam alignment
-        self.seq_hash = None   # Hash the read's sequence
+        self.name = read.qname    # Read name
+        self.seqs = read.query    # The read's sequence
+        self.qual = read.qual     # The read's quality from bam alignment
+        self.mapqual  = read.mapq # Mapping quality to reference genome by BWA
+        self.seq_hash = None      # Hash the read's sequence
     
     def __len__(self):
         return len(self.seqs)
